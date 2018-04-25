cba9543d3ea0a12df6501cb03c10c91b0031a880
Fred Damstra <fred.damstra@gmail.com>
2018-04-25 17:41:52 -0400

    Autodetect IP

--- start_ports.sh
+++ start_ports.sh
@@ -3,12 +3,16 @@
 # Modify these as you see fit
 START_PORT=6000
 END_PORT=6025
+INTERFACE=eth1
+
+# Get the IP of eth1
+IP=$(ifconfig ${INTERFACE} | grep "inet addr" | cut -d: -f2 | awk '{ print $1 }')
 
 mkdir -p logs/tcp
 for i in `seq ${START_PORT} ${END_PORT}`
 do
 	echo Starting server on port $i
-	python server.py --port $i --logfile logs/tcp/$i.log --daemonize --runas nobody
+	python /opt/multithreaded_socket_logger/server.py --ip ${IP} --port $i --logfile /opt/multithreaded_socket_logger/logs/tcp/$i.log --daemonize --runas nobody
 done
 
 exit 0
