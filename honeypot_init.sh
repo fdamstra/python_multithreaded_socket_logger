1cc7660757b9053a4a91c972fc076a25371c9525
Fred Damstra <fred.damstra@gmail.com>
2018-04-26 09:22:57 -0400

    Splunk config

--- honeypot_init.sh
+++ honeypot_init.sh
@@ -13,3 +13,10 @@ chmod 700 /etc/rc.local
 ifconfig eth0 | grep "inet addr" | cut -d: -f2 | awk '{ print "ListenAddress", $1 }' >> /etc/ssh/sshd_config
 service sshd restart
 
+# Grab Splunk UF
+wget -O /usr/local/src/splunkforwarder-7.1.0.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.0&product=universalforwarder&filename=splunkforwarder-7.1.0-2e75b3406c5b-linux-2.6-amd64.deb&wget=true'
+apt-get install -y /usr/local/src/splunkforwarder-7.1.0.deb
+/opt/splunkforwarder/bin/splunk enable boot-start --user splunk --accept-license --seed-passwd "NotTheDefault"
+/opt/splunkforwarder/bin/splunk set deploy-server 10.66.1.10:8089 -auth admin:NotTheDefault
+chown -R splunk:splunk /opt/splunkforwarder
+
--- splunkserver_init.sh
+++ splunkserver_init.sh
@@ -2,6 +2,19 @@
 
 # For the latest, get from splunk.com
 wget -O /usr/local/src/splunk-7.1.0.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.0&product=splunk&filename=splunk-7.1.0-2e75b3406c5b-linux-2.6-amd64.deb&wget=true'
-
 apt-get install -y /usr/local/src/splunk-7.1.0.deb
+sudo -u splunk /opt/splunk/bin/splunk start --accept-license --seed-passwd "NotTheDefault"
+/opt/splunk/bin/splunk enable boot-start --user splunk
+/opt/splunk/bin/splunk enable deploy-server -auth admin:NotTheDefault
+/opt/splunk/bin/splunk enable web-ssl -auth admin:NotTheDefault
+/opt/splunk/bin/splunk stop
+chown -R splunk:splunk /opt/splunk
+
+# Redirect 443 to spunk
+iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8000
+service netfilter-persistent start
+invoke-rc.d netfilter-persistent save
+netfilter-persistent save
+
+
 
